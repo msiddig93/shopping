6fc90a8423068466a2ea7dcdd0dffe8b9297773d
Mohamed Siddig <mohamedsiddig915@gmail.com>
2025-10-02 11:02:17 +0200

    refactor customer login & registeration

--- routes/web.php
+++ routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return Hash::make('password');
+    // return Hash::make('password');
     return view('welcome');
 });
 
